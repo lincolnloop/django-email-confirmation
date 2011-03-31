a76fd0a10e8079b498354decc76de05be4857995
Jason Culverhouse <jason@mischievous.org>
2011-03-31 15:30:13 -0700

    add success message and optional success redirect

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -1,12 +1,16 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from emailconfirmation.models import EmailConfirmation
 
 
-def confirm_email(request, confirmation_key):
+def confirm_email(request, confirmation_key, success_url=None):
     confirmation_key = confirmation_key.lower()
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
+    if email_address and success_url:
+        messages.success(request, "Thanks for confirming you email.")
+        return HttpResponseRedirect(success_url)
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
     }, context_instance=RequestContext(request))
