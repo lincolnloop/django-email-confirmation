2b33a3941e38e61e1a26511e0f282aa21e99b676
Chris Beaven <smileychris@gmail.com>
2011-05-16 16:27:15 +1200

    Make the success message translatable

--- emailconfirmation/views.py
+++ emailconfirmation/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
 from emailconfirmation.models import EmailConfirmation
 
@@ -10,7 +11,7 @@ def confirm_email(request, confirmation_key, success_url=None):
     confirmation_key = confirmation_key.lower()
     email_address = EmailConfirmation.objects.confirm_email(confirmation_key)
     if email_address and success_url:
-        messages.success(request, "Thanks for confirming your email.")
+        messages.success(request, _("Thanks for confirming your email."))
         return HttpResponseRedirect(success_url)
     return render_to_response("emailconfirmation/confirm_email.html", {
         "email_address": email_address,
